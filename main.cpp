37839ca9b439edd520b7bb286e87dc7cffb43a09
Yeshasvitvs <yeshasvitvs@gmail.com>
2017-03-29 13:30:03 +0200

    using aruco from opencv3.1 build

--- main.cpp
+++ main.cpp
@@ -4,12 +4,8 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
-#include <aruco/aruco.h>
-#include <aruco/dictionary.h>
-#include <aruco/marker.h>
-#include <aruco/cvdrawingutils.h>
-
-using namespace aruco;
+#include <opencv2/aruco/charuco.hpp>
+#include <opencv2/aruco/dictionary.hpp>
 
 int main(int argc, char **argv) {
     
@@ -28,22 +24,17 @@ int main(int argc, char **argv) {
     bool init = false;
     int n_markers = 50;
     
-    aruco::MarkerDetector MDetector;
     std::vector<cv::Mat>* marker_images;
     marker_images = new std::vector<cv::Mat>(n_markers);
-    //aruco::Dictionary marker_dict = getPredefinedDictionary(DICT_6X6_50);
-    //aruco::Dictionary marker_dict = aruco::Dictionary::loadPredefined("ARUCO_MIP_36h12");
-    //float MarkerSize = 0.5;
+    cv::aruco::Dictionary marker_dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     
-    //No need to draw images for ARUCO_MIP_36h12 - downloaded
-    /*if(init != true)
+    if(init != true)
     {
         for(int i = 0; i < n_markers; i++)
         {
             std::cout << "Saving marker : " << i << std::endl;
             cv::Mat marker_image = marker_images->at(i);
-            marker_dict
-            aruco::drawMarker(marker_dict,i,200,marker_image);
+            cv::aruco::drawMarker(marker_dict,i,200,marker_image);
             std::string file_name = boost::lexical_cast<std::string>(i) + ".jpg";
             std::string file_location = marker_directory + "/" + file_name;
             std::cout << "file location : " << file_location;
@@ -53,7 +44,7 @@ int main(int argc, char **argv) {
             }
         }
         init = true;
-    }*/
+    }
     
     //Capturing camera input
     cv::VideoCapture inputVideo;
@@ -67,29 +58,26 @@ int main(int argc, char **argv) {
     fs["camera_matrix"] >> camera_matrix;
     fs["distortion_coefficients"] >> dist_coeffs;
     fs.release();
-    
-    aruco::CameraParameters camera_parameters;
-    camera_parameters.readFromXMLFile(calibration_file_);
+
     
     //Marker detection and pose estimation
-    /*bool loop = true;
     std::vector<cv::Vec3d> rvecs, tvecs;
     std::vector<int> markersIds;
     std::vector< std::vector<cv::Point2f> > markerCorners, rejectedCandidates;
-    DetectorParameters parameters;
-    while(loop)
+    cv::aruco::DetectorParameters parameters;
+    while(1)
     {
         cv::Mat inputImage;
         inputVideo.read(inputImage);
         
-        detectMarkers(inputImage, marker_dict, markerCorners, markersIds, parameters, rejectedCandidates);
-        drawDetectedMarkers(inputImage,markerCorners,markersIds);
-        estimatePoseSingleMarkers(markerCorners,0.05,camera_matrix,dist_coeffs,rvecs,tvecs);
+        cv::aruco::detectMarkers(inputImage, marker_dict, markerCorners, markersIds, parameters, rejectedCandidates);
+        cv::aruco::drawDetectedMarkers(inputImage,markerCorners,markersIds);
+        cv::aruco::estimatePoseSingleMarkers(markerCorners,0.05,camera_matrix,dist_coeffs,rvecs,tvecs);
         
         for(int i=0; i < markersIds.size(); i++)
         {
             //Pose values of each marker
-            drawAxis(inputImage,camera_matrix,dist_coeffs,rvecs[i],tvecs[i],0.1);
+            cv::aruco::drawAxis(inputImage,camera_matrix,dist_coeffs,rvecs[i],tvecs[i],0.1);
         }
         cv::namedWindow("Input Image",CV_WINDOW_AUTOSIZE);
         cv::imshow("Input Image",inputImage);
@@ -99,21 +87,45 @@ int main(int argc, char **argv) {
             std::cout << "Esc key pressed!" << std::endl;
             break; 
         }
-    }*/
-    //MDetector.setThresholdParams(7,7);
-    //MDetector.setThresholdParamRange(2,0);
-    MDetector.setDictionary("ARUCO_MIP_36h12");
-    std::map<uint32_t,aruco::MarkerPoseTracker> MTracker;
+    }
+    
+    return 0;
+}
+
+//Implementaion using only aruco library without
+/*
+ * 
+ * #include <aruco/aruco.h>
+#include <aruco/dictionary.h>
+#include <aruco/marker.h>
+#include <aruco/cvdrawingutils.h>
+
+using namespace aruco;
+aruco::MarkerDetector MDetector;
+MDetector.setThresholdParams(7,7);
+MDetector.setThresholdParamRange(2,0);
+MDetector.setDictionary("ARUCO_MIP_36h12");
+
+//aruco::Dictionary marker_dict = aruco::Dictionary::loadPredefined("ARUCO_MIP_36h12");
+
+std::string calibration_file_ = "/home/yeshi/projects/aruco_markers/out_camera_data.yml";
+aruco::CameraParameters camera_parameters;
+camera_parameters.readFromXMLFile(calibration_file_);
+std::cout << camera_parameters.CamSize << std::endl;
+std::cout << camera_parameters.CameraMatrix << std::endl;
+std::cout << camera_parameters.Distorsion << std::endl;
+
+std::map<uint32_t,aruco::MarkerPoseTracker> MTracker;
     cv::Mat inputImage;
     while(1)
     {
         inputVideo.read(inputImage);
-        std::vector<aruco::Marker> markers = MDetector.detect(inputImage,camera_parameters,1,false);
+        std::vector<aruco::Marker> markers = MDetector.detect(inputImage);
         //inputImage = MDetector.getThresholdedImage(); //This works
         //std::cout << "Detected markers : " << markers.size() << std::endl;
         for(int i=0; i < markers.size(); i++)
         {
-            MTracker[i].estimatePose(markers.at(i),camera_parameters,-1);
+            MTracker[i].estimatePose(markers.at(i),camera_parameters,-1,4);
             //markers[i].draw(inputImage,cv::Scalar(0,0,255),2);
             aruco::CvDrawingUtils::draw3dAxis(inputImage,markers[i],camera_parameters);
         }
@@ -127,8 +139,5 @@ int main(int argc, char **argv) {
         }
         
     }
-    
-    
-    
-    return 0;
-}
+
+*/
