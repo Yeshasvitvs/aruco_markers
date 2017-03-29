5f5587b56b33fecf70f4be22add867b0d644293d
Yeshasvitvs <yeshasvitvs@gmail.com>
2017-03-29 19:39:30 +0200

    corner detection parameters tuning- smooth pose estimates

--- main.cpp
+++ main.cpp
@@ -27,22 +27,24 @@ int main(int argc, char **argv) {
     
     //Marker generation
     bool init = false;
-    int n_markers = 50;
+    int n_markers = 10;
+    int marker_size = 5;; 
     
     std::vector<cv::Mat>* marker_images;
     marker_images = new std::vector<cv::Mat>(n_markers);
-    cv::aruco::Dictionary marker_dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_50);
-    
+    cv::aruco::Dictionary marker_dict = cv::aruco::generateCustomDictionary(n_markers,marker_size);
+    float markerBorder_bits = 1;
+    int marker_pixel_resol = 300; 
     if(init != true)
     {
         for(int i = 0; i < n_markers; i++)
         {
-            std::cout << "Saving marker : " << i << std::endl;
+            //std::cout << "Saving marker : " << i << std::endl;
             cv::Mat marker_image = marker_images->at(i);
-            cv::aruco::drawMarker(marker_dict,i,200,marker_image);
+            cv::aruco::drawMarker(marker_dict,i,marker_pixel_resol,marker_image,markerBorder_bits);
             std::string file_name = boost::lexical_cast<std::string>(i) + ".jpg";
             std::string file_location = marker_directory + "/" + file_name;
-            std::cout << "file location : " << file_location;
+            std::cout << "file location : " << file_location << std::endl;
             while(!cv::imwrite(file_location,marker_image))
             {
                 continue;
@@ -52,8 +54,10 @@ int main(int argc, char **argv) {
     }
     
     //Capturing camera input
-    cv::VideoCapture inputVideo;
-    inputVideo.open(0);
+    cv::VideoCapture inputVideo_cap;
+    inputVideo_cap.open(0);
+    inputVideo_cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
+    inputVideo_cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
     
     //Camera calibration
     cv::Mat camera_matrix, dist_coeffs;
@@ -70,19 +74,59 @@ int main(int argc, char **argv) {
     std::vector<int> markersIds;
     std::vector< std::vector<cv::Point2f> > markerCorners, rejectedCandidates;
     cv::aruco::DetectorParameters parameters;
+    
+    //Thresholding parameters
+    /*parameters.adaptiveThreshWinSizeMin = 10;
+    parameters.adaptiveThreshWinSizeMax = 30;
+    parameters.adaptiveThreshWinSizeStep = 3;
+    parameters.adaptiveThreshConstant = 7;*/
+    
+    //Contour Filtering parameters
+    /*parameters.minMarkerPerimeterRate = 30/inputVideo_cap.get(CV_CAP_PROP_FRAME_WIDTH); //Specified relative to the max dimension of input image 640x0.05 = 32 pixels
+    parameters.maxMarkerPerimeterRate = 60/inputVideo_cap.get(CV_CAP_PROP_FRAME_WIDTH);
+    
+    parameters.minCornerDistanceRate = 0.05;
+    parameters.minMarkerDistanceRate = 0.05;
+    parameters.minDistanceToBorder = 3;*/
+   
+    //Bits extraction
+    /*parameters.markerBorderBits = markerBorder_bits;
+    parameters.minOtsuStdDev = 2;
+    parameters.perspectiveRemovePixelPerCell = 10;
+    parameters.perspectiveRemoveIgnoredMarginPerCell = 0.2;*/
+    
+    //Marker Identification
+    parameters.maxErroneousBitsInBorderRate = 0.2; //Relative to the total number of bits
+    parameters.errorCorrectionRate = 0.6;
+    
+    //Corner Refinement
+    parameters.doCornerRefinement = true;
+    parameters.cornerRefinementWinSize = 5;
+    parameters.cornerRefinementMaxIterations = 100;
+    parameters.cornerRefinementMinAccuracy = 0.1;
+    
+    float marker_size_in_meters = 0.05;
+    float axis_length = 0.02;
+    
+    cv::Mat inputImage, outputImage;
+    
+    
     while(1)
     {
-        cv::Mat inputImage;
-        inputVideo.read(inputImage);
+        
+        inputVideo_cap.read(inputImage);
+        
         
         cv::aruco::detectMarkers(inputImage, marker_dict, markerCorners, markersIds, parameters, rejectedCandidates);
+        if(markersIds.empty())
+            std::cout << "Cannot detect markers" << std::endl;
         cv::aruco::drawDetectedMarkers(inputImage,markerCorners,markersIds);
-        cv::aruco::estimatePoseSingleMarkers(markerCorners,0.1,camera_matrix,dist_coeffs,rvecs,tvecs);
+        cv::aruco::estimatePoseSingleMarkers(markerCorners,0.05,camera_matrix,dist_coeffs,rvecs,tvecs);
         
         for(int i=0; i < markersIds.size(); i++)
         {
             //Pose values of each marker
-            cv::aruco::drawAxis(inputImage,camera_matrix,dist_coeffs,rvecs[i],tvecs[i],0.1);
+            cv::aruco::drawAxis(inputImage,camera_matrix,dist_coeffs,rvecs[i],tvecs[i],axis_length);
         }
         cv::namedWindow("Input Image",CV_WINDOW_AUTOSIZE);
         cv::imshow("Input Image",inputImage);
@@ -92,6 +136,8 @@ int main(int argc, char **argv) {
             std::cout << "Esc key pressed!" << std::endl;
             break; 
         }
+        markersIds.clear();
+        markerCorners.clear();
     }
     
     return 0;
